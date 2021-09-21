4affc2c4f30d70206e836e8b71dee55c44e01fd9
andrewmoquia <andrewmoquia09@gmail.com>
2021-09-21 15:20:57 +0800

    Change route.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,9 @@
 import '../scss/main.scss'
-import Nav from './Nav'
 import Dashboard from './Dashboard'
 import Marketplace from './Marketplace'
 import Farm from './Farm'
+import MyNFT from './MyNFT'
+import Nav from './Nav'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -13,8 +14,9 @@ export default function App() {
             <Nav />
             <Switch>
                <Route path="/" exact component={Dashboard}></Route>
-               <Route path="/" exact component={Marketplace}></Route>
-               <Route path="/" exact component={Farm}></Route>
+               <Route path="/marketplace" exact component={Marketplace}></Route>
+               <Route path="/farm" exact component={Farm}></Route>
+               <Route path="/mynft" exact component={MyNFT}></Route>
             </Switch>
          </main>
       </BrowserRouter>
