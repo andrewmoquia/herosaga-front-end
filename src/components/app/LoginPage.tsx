2feea0e8f7ada550c43cf3bf3b0afb0c30691a0c
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-10 17:19:30 +0800

    Clear Login content temp for testing.

--- src/components/app/LoginPage.tsx
+++ src/components/app/LoginPage.tsx
@@ -1,21 +1,13 @@
-import { MainStore } from '../reduceStore/StoreProvider'
 import { useContext } from 'react'
+import { MainStore } from '../reduceStore/StoreProvider'
 
 export default function Login() {
-   const { dispatch } = useContext(MainStore)
-
-   const handleLogin = () => {
-      return dispatch({ type: 'AUTH_SUCCESS' })
-   }
-
+   const { state } = useContext(MainStore)
+   console.log(state)
    return (
       <div>
-         <h1>Login</h1>
-         <form action="/" onSubmit={() => handleLogin()}>
-            <input type="text" name="username" id="username" placeholder="Username" required />
-            <input type="password" name="password" id="password" placeholder="Password" required />
-            <button type="submit">submit</button>
-         </form>
+         {/* <h1>Login</h1> */}
+         <button>Login</button>
       </div>
    )
 }
