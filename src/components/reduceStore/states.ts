1f3e1f7d0449109cfc7bd6930011d4216ba263cd
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-10 21:09:43 +0800

    Changes lines in state for testing.

--- src/components/reduceStore/states.ts
+++ src/components/reduceStore/states.ts
@@ -1,30 +1,6 @@
-export const userState = {
-   user: {
-      username: 'RQmPIIgAn8wMGOHBinjHmMzf1',
-      authData: {
-         moralisEth: {
-            id: '0x6e9f6689ce9e5f33c72f8ece4f0475528898a3b6',
-            signature:
-               '0x14a191d17917936378851c5c8206f6af7f4637292930492ad32d40636d92fa690b03cd8cdd8ab4477a7b0efd9028174d7802daca909a937ae4863bebe62f24fd1c',
-            data: 'Moralis Authentication',
-         },
-      },
-      createdAt: '2021-09-22T10:13:48.108Z',
-      updatedAt: '2021-09-24T13:38:19.069Z',
-      accounts: ['0x6e9f6689ce9e5f33c72f8ece4f0475528898a3b6'],
-      ethAddress: '0x6e9f6689ce9e5f33c72f8ece4f0475528898a3b6',
-      ACL: {
-         QYbonT3tihkDYgC399IdqYgf: {
-            read: true,
-            write: true,
-         },
-      },
-      sessionToken: 'r:674e0deaf7932d278eadc39ac7a2113b',
-      objectId: 'QYbonT3tihkDYgC399IdqYgf',
-   },
-}
+export const userState = {}
 
 export const authState = {
-   isAuthenticated: true,
+   isAuthenticated: false,
    isAuthenticating: false,
 }
