7c4f934a0da9261d858609a3f3f0349d773ee5bb
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-10 21:06:58 +0800

    Added basic logout function.

--- src/components/app/Dashboard.tsx
+++ src/components/app/Dashboard.tsx
@@ -1,8 +1,21 @@
+import { MainStore } from '../reduceStore/StoreProvider'
+import { useContext } from 'react'
+
 export default function Dashboard() {
+   const { state, dispatch } = useContext(MainStore)
+
+   const handleLogout = () => {
+      console.log('log out trigger', state)
+      return dispatch({
+         type: 'LOGOUT_SUCCESS',
+      })
+   }
    return (
       <section className="main-bg">
          <h1>Dashboard</h1>
-         <button type="button">Logout</button>
+         <button type="button" onClick={() => handleLogout()}>
+            Logout
+         </button>
       </section>
    )
 }
