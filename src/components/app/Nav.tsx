b2bb8a66fd171ae54fc821d35ee0e7e4a96401b2
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-12 15:27:18 +0800

    Separate main nav and menu nav.

--- src/components/app/Nav.tsx
+++ src/components/app/Nav.tsx
@@ -1,50 +1,14 @@
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { NavLiElement, NavMenus } from '../interfaces/navInterface'
+import { useState } from 'react'
+import { NavMenus } from '../interfaces/navInterface'
+import { navMenuLogoData } from '../data/navMenuLogo'
+import NavMenu from './NavMenu'
 
 export default function Nav() {
-   const navLiElement: NavLiElement = {
-      dashboard: '',
-      marketplace: '',
-      farm: '',
-      myNFT: '',
-      profile: '',
-      transactions: '',
-   }
-
    const navMenus: NavMenus = {
       class: 'menu-change-to-main',
       status: true,
    }
-
-   const [activeLi, setActiveLi] = useState<NavLiElement>(navLiElement)
    const [activeMenu, setActiveMenu] = useState<NavMenus>(navMenus)
-
-   //Change Nav Li class when activated.
-   const mapLiElements = (route: string) => {
-      // console.log('menu changed')
-      const newObj: any = activeLi
-      Object.keys(newObj).map((key) => {
-         return route === key
-            ? (newObj[`${key}`] = 'active-nav')
-            : route === ''
-            ? (newObj[`dashboard`] = 'active-nav')
-            : (newObj[`${key}`] = 'inactive-nav')
-      })
-      setActiveLi((prevObj: NavLiElement) => {
-         return { ...prevObj, ...newObj }
-      })
-   }
-
-   //Identify which page we are.
-   useEffect(() => {
-      mapLiElements(window.location.href.slice(22))
-      //eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [])
-
-   const handleActiveNav = (route: string) => {
-      mapLiElements(route)
-   }
 
    const changeMenuClass = (newObj: NavMenus) => {
       setActiveMenu((prevObj: NavMenus) => {
@@ -70,6 +34,14 @@ export default function Nav() {
       }
    }
 
+   const generalMenuProps = {
+      data: navMenuLogoData.generalMenu,
+   }
+
+   const profileMenuProps = {
+      data: navMenuLogoData.profileMenu,
+   }
+
    return (
       <nav id="main-nav">
          {/* Game Logo  */}
@@ -81,82 +53,10 @@ export default function Nav() {
          </div>
          <div className="game-logo d-none d-sm-flex">INCU MONSTERS</div>
          <div id="nav-menu">
-            {/* Profile Menu  */}
-            <ul className={`menu main`}>
-               <Link to="/" className={activeLi.dashboard}>
-                  <li onClick={() => handleActiveNav('dashboard')}>
-                     <div className="nav-icon">
-                        <img
-                           src="https://i.ibb.co/8c0hdC6/UI-Graphic-Resource-Gems.png"
-                           alt="UI-Graphic-Resource-Gems"
-                        />
-                     </div>
-                     <p className="d-none d-lg-block">Dashboard</p>
-                  </li>
-               </Link>
-               <Link to="/marketplace" className={activeLi.marketplace}>
-                  <li onClick={() => handleActiveNav('marketplace')}>
-                     <div className="nav-icon">
-                        <img
-                           src="https://i.ibb.co/zNmxRH1/UI-Graphic-Resource-Coins.png"
-                           alt="UI-Graphic-Resource-Coins"
-                        />
-                     </div>
-                     <p className="d-none d-lg-block">Marketplace</p>
-                  </li>
-               </Link>
-               <Link to="/farm" className={activeLi.farm}>
-                  <li onClick={() => handleActiveNav('farm')}>
-                     <div className="nav-icon">
-                        <img
-                           src="https://i.ibb.co/fDB1Tsf/UI-Graphic-Resource-Wood.png"
-                           alt="UI-Graphic-Resource-Wood"
-                        />
-                     </div>
-                     <p className="d-none d-lg-block">Farm</p>
-                  </li>
-               </Link>
-               <Link to="/myNFT" className={activeLi.myNFT}>
-                  <li onClick={() => handleActiveNav('myNFT')}>
-                     <div className="nav-icon">
-                        <img
-                           src="https://i.ibb.co/JydCdMs/UI-Graphic-Resource-Iron.png"
-                           alt="UI-Graphic-Resource-Iron"
-                        />
-                     </div>
-                     <p className="d-none d-lg-block">My NFT</p>
-                  </li>
-               </Link>
-            </ul>
             {/* Main Menu  */}
-            <ul className={`menu main`}>
-               <Link to="/" className={activeLi.profile}>
-                  <li onClick={() => handleActiveNav('profile')}>
-                     <div className="nav-icon">
-                        <div className="nav-icon">
-                           <img src="https://i.ibb.co/thCCPfj/Menu.png" alt="Menu" />
-                        </div>
-                     </div>
-                     <p className="d-none d-lg-block">Profile</p>
-                  </li>
-               </Link>
-               <Link to="/marketplace" className={activeLi.transactions}>
-                  <li onClick={() => handleActiveNav('transactions')}>
-                     <div className="nav-icon">
-                        <img src="https://i.ibb.co/2y06sSm/Reload.png" alt="Reload" />
-                     </div>
-                     <p className="d-none d-lg-block">Transactions</p>
-                  </li>
-               </Link>
-               <Link to="/farm">
-                  <li>
-                     <div className="nav-icon">
-                        <img src="https://i.ibb.co/mGY9ncP/Pause.png" alt="Pause" />
-                     </div>
-                     <p className="d-none d-lg-block">Logout</p>
-                  </li>
-               </Link>
-            </ul>
+            <NavMenu {...generalMenuProps} />
+            {/* Profile Menu  */}
+            <NavMenu {...profileMenuProps} />
             {/* Empty menu for animation purposes */}
             <ul className={activeMenu.class}> </ul>
          </div>
---
+++ src/components/app/NavMenu.tsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { NavLiElement } from '../interfaces/navInterface'
+
+export default function NavMenu(props: any): any {
+   const { data } = props
+
+   const navLiElement: NavLiElement = {
+      dashboard: '',
+      marketplace: '',
+      farm: '',
+      myNFT: '',
+      profile: '',
+      transactions: '',
+   }
+   const [activeLi, setActiveLi] = useState<NavLiElement>(navLiElement)
+
+   // Change Nav Li class when activated.
+   const mapLiElements = (route: string) => {
+      // console.log('menu changed')
+      const newObj: any = activeLi
+      Object.keys(newObj).map((key) => {
+         return route === key
+            ? (newObj[`${key}`] = 'active-nav')
+            : route === ''
+            ? (newObj[`dashboard`] = 'active-nav')
+            : (newObj[`${key}`] = 'inactive-nav')
+      })
+      setActiveLi((prevObj: NavLiElement) => {
+         return { ...prevObj, ...newObj }
+      })
+   }
+   // //Identify which page we are.
+   useEffect(() => {
+      mapLiElements(window.location.href.slice(22))
+      //eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [])
+
+   const handleActiveNav = (route: string) => {
+      mapLiElements(route)
+   }
+
+   return (
+      <ul className={`menu main`}>
+         {data.map((key: any) => {
+            return (
+               <Link to={`/${key.type[0]}`} className={activeLi[key.type[0]]} key={key.type}>
+                  <li onClick={() => handleActiveNav(key.type)}>
+                     <div className="nav-icon">
+                        <img src={key.img} alt={key.alt} />
+                     </div>
+                     <p className="d-none d-lg-block">{key.type[1]}</p>
+                  </li>
+               </Link>
+            )
+         })}
+      </ul>
+   )
+}
