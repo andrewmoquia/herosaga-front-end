3238e92a1d1c1ec03467ea34988ee515a0ca78e0
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-12 15:26:33 +0800

    Added hard coded data for navigation.

---
+++ src/components/data/navMenuLogo.ts
@@ -0,0 +1,41 @@
+export const navMenuLogoData = {
+   generalMenu: [
+      {
+         type: ['dashboard', 'Dashboard'],
+         img: 'https://i.ibb.co/8c0hdC6/UI-Graphic-Resource-Gems.png',
+         alt: 'Spell-Book-Preface-14',
+      },
+      {
+         type: ['marketplace', 'Marketplace'],
+         img: 'https://i.ibb.co/zNmxRH1/UI-Graphic-Resource-Coins.png',
+         alt: 'UI-Graphic-Resource-Wood',
+      },
+      {
+         type: ['farm', 'Farm'],
+         img: 'https://i.ibb.co/fDB1Tsf/UI-Graphic-Resource-Wood.png',
+         alt: 'UI-Graphic-Resource-Wood',
+      },
+      {
+         type: ['myNFT', 'My NFT'],
+         img: 'https://i.ibb.co/JydCdMs/UI-Graphic-Resource-Iron.png',
+         alt: 'UI-Graphic-Resource-Iron',
+      },
+   ],
+   profileMenu: [
+      {
+         type: ['profile', 'Profile'],
+         img: 'https://i.ibb.co/thCCPfj/Menu.png',
+         alt: 'Profile logo.',
+      },
+      {
+         type: ['transactions', 'Transactions'],
+         img: 'https://i.ibb.co/2y06sSm/Reload.png',
+         alt: 'Transaction logo.',
+      },
+      {
+         type: ['logout', 'Logout'],
+         img: 'https://i.ibb.co/mGY9ncP/Pause.png',
+         alt: 'Logout logo.',
+      },
+   ],
+}
