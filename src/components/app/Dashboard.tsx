7fd0476b79722748448484ae1afa0a5af63da21c
andrewmoquia <andrewmoquia09@gmail.com>
2021-10-24 09:28:27 +0800

    Update logout function.

--- src/components/app/Dashboard.tsx
+++ src/components/app/Dashboard.tsx
@@ -1,19 +1,32 @@
 import { MainStore } from '../reduceStore/StoreProvider'
 import { useContext } from 'react'
+import axios from 'axios'
 
 export default function Dashboard() {
-   const { state, dispatch } = useContext(MainStore)
+   const { dispatch } = useContext(MainStore)
 
-   const handleLogout = () => {
-      console.log('log out trigger', state)
-      return dispatch({
-         type: 'LOGOUT_SUCCESS',
-      })
+   const handleLogout = (e: any) => {
+      e.preventDefault()
+      axios
+         .get('http://localhost:5000/logout', {
+            withCredentials: true,
+         })
+         .then((response) => {
+            console.log(response.data)
+            if (response.data.status === 200) {
+               return dispatch({
+                  type: 'LOGOUT_SUCCESS',
+               })
+            }
+         })
+         .catch((error) => {
+            console.log(error)
+         })
    }
    return (
       <section className="main-bg">
          <h1>Dashboard</h1>
-         <button type="button" onClick={() => handleLogout()}>
+         <button type="button" onClick={(e) => handleLogout(e)}>
             Logout
          </button>
       </section>
