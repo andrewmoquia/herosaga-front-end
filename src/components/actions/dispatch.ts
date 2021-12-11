8620b2e550d7a149f7aee8f4a7fd210e6963260b
andrewmoquia <andrewmoquia09@gmail.com>
2021-12-12 01:09:27 +0800

    Added 60s timer func.

--- src/components/actions/dispatch.ts
+++ src/components/actions/dispatch.ts
@@ -4,3 +4,15 @@ export const runDispatch = (dispatch: Function, type: string, payload: any) => {
       payload,
    })
 }
+
+export const sixtySecTimer = (dispatch: any) => {
+   //Start 60s timer to make request again.
+   const timer = setInterval(() => {
+      runDispatch(dispatch, 'RUN_REQ_TIMER', '')
+   }, 1000)
+   //If timer is zero allow user to send request to change pw again.
+   return setTimeout(() => {
+      clearInterval(timer)
+      return runDispatch(dispatch, 'REQUEST_TIMEOUT', '')
+   }, 60000)
+}
