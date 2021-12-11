8e4885e7ca6fd01d9a6524cb23926a0b6e2347d6
andrewmoquia <andrewmoquia09@gmail.com>
2021-12-11 11:43:32 +0800

    Moved into designated folder.

---
+++ src/components/app/dashboards/Marketplace.tsx
@@ -0,0 +1,7 @@
+export default function Marketplace() {
+   return (
+      <section className="main-bg">
+         <h1>Marketplace</h1>
+      </section>
+   )
+}
---
+++ src/components/app/dashboards/MyNFT.tsx
@@ -0,0 +1,7 @@
+export default function MyNFT() {
+   return (
+      <section className="main-bg">
+         <h1>My NFTS</h1>
+      </section>
+   )
+}
