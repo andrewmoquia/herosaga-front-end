cc96979323a67ac86192febb95415676e6656ce1
andrewmoquia <andrewmoquia09@gmail.com>
2022-01-19 03:34:16 +0800

    Fixed unchanged route.

--- src/components/app/microsite/ViewNFT.tsx
+++ src/components/app/microsite/ViewNFT.tsx
@@ -34,7 +34,7 @@ export default function ViewNFT() {
    //Sent request to get nft through id
    const getNFTData = useCallback(() => {
       axios
-         .get(`http://localhost:5000/${currDashboard}/nft/${id}`, {
+         .get(`https://incumons.herokuapp.com/${currDashboard}/nft/${id}`, {
             withCredentials: true,
          })
          .then((res) => {
